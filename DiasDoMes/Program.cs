e0a7e30c965cae9349d3b4f7321d872a52c16b08
Ramon Chiara <ramon@ramon.pro.br>
2017-09-27 22:54:07 -0300

    Add project files.

---
+++ DiasDoMes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiasDoMes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] diasDoMes = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            // ENTRADA
+            Console.Write("Mês: ");
+            int mes = int.Parse(Console.ReadLine());
+
+            // PROCESSAMENTO
+            int dias = diasDoMes[mes - 1];
+
+            // SAÍDA
+            Console.WriteLine("Esse mês tem {0} dias", dias);
+        }
+    }
+}
